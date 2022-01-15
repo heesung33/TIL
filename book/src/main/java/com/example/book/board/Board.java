91aa8f8e22070c235c6ffe8e07c6e06a3688d30c
heesung33 <chaeheesung33@gmail.com>
2022-01-16 03:06:29 +0900

    [UPDATE] Board

---
+++ book/src/main/java/com/example/book/board/Board.java
@@ -0,0 +1,38 @@
+package com.example.book.board;
+
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String bookname;
+
+    @Column(nullable = false)
+    private String bookmaker;
+
+    @Column(nullable = false)
+    private String  creater;
+
+    @Builder
+    public Board(String bookname, String bookmaker, String creater) {
+        this.bookmaker=bookmaker;
+        this.bookname=bookname;
+        this.creater=creater;
+
+    }
+
+
+
+
+}
