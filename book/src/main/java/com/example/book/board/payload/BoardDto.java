35fff34e7c5ed1dca91f29939a4819e37c278c00
heesung33 <chaeheesung33@gmail.com>
2022-01-16 03:07:21 +0900

    [UPDATE] BoardDto

---
+++ book/src/main/java/com/example/book/board/payload/BoardDto.java
@@ -0,0 +1,30 @@
+package com.example.book.board.payload;
+
+import com.example.book.board.Board;
+import lombok.*;
+
+@Getter
+@ToString
+@Setter
+@NoArgsConstructor
+public class BoardDto {
+    private String bookmaker;
+    private String bookname;
+    private String creater;
+
+
+    public Board toEntity() {
+        return Board.builder()
+                .bookmaker(bookmaker)
+                .bookname(bookname)
+                .creater(creater)
+                .build();
+    }
+
+    @Builder
+    public BoardDto(String bookname, String bookmaker, String creater){
+        this.bookmaker=bookmaker;
+        this.bookname=bookname;
+        this.creater=creater;
+    }
+}
