9388d4e6b74b7fe72af2a9c48704ec2c296c0bdd
heesung33 <chaeheesung33@gmail.com>
2022-03-29 21:46:44 +0900

    🔫 AuthService

--- jwt/src/main/java/com/example/jwt/service/AuthService.java
+++ jwt/src/main/java/com/example/jwt/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.example.jwt.service;
 
+import com.example.jwt.payload.LoginRequest;
 import com.example.jwt.payload.SignupRequest;
+import com.example.jwt.payload.TokenResponse;
 
 public interface AuthService {
     void signup(SignupRequest signupRequest);
-
+    TokenResponse signin(LoginRequest loginRequest);
+    TokenResponse reissue(String token);
 }
