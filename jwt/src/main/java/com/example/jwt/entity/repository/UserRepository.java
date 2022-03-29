cd61878d247c88a5c78a35c32dc1117eca560c45
heesung33 <chaeheesung33@gmail.com>
2022-03-29 21:44:18 +0900

    🔫 UserRepository

--- jwt/src/main/java/com/example/jwt/entity/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jwt.entity.repository;
-
-import com.example.jwt.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, Long>{
-}
