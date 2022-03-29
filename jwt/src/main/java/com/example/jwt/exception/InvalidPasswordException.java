ff18f0beb94e6835375a6c8d1978388bb60475bd
heesung33 <chaeheesung33@gmail.com>
2022-03-29 21:47:55 +0900

    🔫 InvalidPasswordException

---
+++ jwt/src/main/java/com/example/jwt/exception/InvalidPasswordException.java
@@ -0,0 +1,12 @@
+package com.example.jwt.exception;
+
+public class InvalidPasswordException extends BusinessException {
+
+    public static BusinessException EXCEPTTION =
+            new InvalidPasswordException();
+
+
+    private InvalidPasswordException(){
+        super(ErrorCode.INVALID_PASSWORD);
+    }
+}
