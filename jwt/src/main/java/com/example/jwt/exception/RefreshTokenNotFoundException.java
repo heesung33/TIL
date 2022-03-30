fcc316001d1ecf2443b545e060d952809de9f1d5
heesung33 <chaeheesung33@gmail.com>
2022-03-30 10:02:16 +0900

    ⚙️ 예외처리 구조 수정

--- jwt/src/main/java/com/example/jwt/exception/RefreshTokenNotFoundException.java
+++ jwt/src/main/java/com/example/jwt/exception/RefreshTokenNotFoundException.java
@@ -1,7 +1,11 @@
 package com.example.jwt.exception;
 
 public class RefreshTokenNotFoundException extends BusinessException {
-    public RefreshTokenNotFoundException() {
+
+    public static BusinessException EXCEPTION =
+            new RefreshTokenNotFoundException();
+
+    private RefreshTokenNotFoundException(){
         super(ErrorCode.REFRESH_TOKEN_NOT_FOUND);
     }
 }
