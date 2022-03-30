32f65b648e30f4ba9c7b3f4145e5c4992ff9aadb
heesung33 <chaeheesung33@gmail.com>
2022-03-30 10:02:22 +0900

    ⚙️ 예외처리 구조 수정

--- jwt/src/main/java/com/example/jwt/exception/IncorrectTokenException.java
+++ jwt/src/main/java/com/example/jwt/exception/IncorrectTokenException.java
@@ -1,7 +1,12 @@
 package com.example.jwt.exception;
 
 public class IncorrectTokenException extends BusinessException {
-    public IncorrectTokenException() {
+
+    public static BusinessException EXCEPTION =
+            new IncorrectTokenException();
+
+
+    private IncorrectTokenException(){
         super(ErrorCode.INCORRECT_TOKEN);
     }
 }
