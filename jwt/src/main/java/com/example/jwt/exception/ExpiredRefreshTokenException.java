99191f4536eaded69a7adbfa28c92fe36bbd5788
heesung33 <chaeheesung33@gmail.com>
2022-03-30 10:02:20 +0900

    ⚙️ 예외처리 구조 수정

--- jwt/src/main/java/com/example/jwt/exception/ExpiredRefreshTokenException.java
+++ jwt/src/main/java/com/example/jwt/exception/ExpiredRefreshTokenException.java
@@ -1,7 +1,12 @@
 package com.example.jwt.exception;
 
 public class ExpiredRefreshTokenException extends BusinessException {
-    public ExpiredRefreshTokenException() {
+
+    public static BusinessException EXCEPTION =
+            new ExpiredRefreshTokenException();
+
+    private ExpiredRefreshTokenException(){
         super(ErrorCode.EXPIRED_REFRESH_TOKEN);
     }
+
 }
