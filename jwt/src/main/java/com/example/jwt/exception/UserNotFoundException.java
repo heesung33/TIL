fe07f2c409b4eefe2ee5a67b8891e26139b1fda4
heesung33 <chaeheesung33@gmail.com>
2022-03-30 10:02:28 +0900

    ⚙️ 예외처리 구조 수정

--- jwt/src/main/java/com/example/jwt/exception/UserNotFoundException.java
+++ jwt/src/main/java/com/example/jwt/exception/UserNotFoundException.java
@@ -1,7 +1,11 @@
 package com.example.jwt.exception;
 
 public class UserNotFoundException extends BusinessException{
-    public UserNotFoundException(){
+
+    public static BusinessException EXCEPTION =
+            new UserNotFoundException();
+
+    private UserNotFoundException(){
         super(ErrorCode.USER_NOT_FOUND);
     }
 }
