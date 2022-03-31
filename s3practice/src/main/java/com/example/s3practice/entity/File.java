82de93304b83201686de8b0be214b41b9dc601e3
heesung33 <chaeheesung33@gmail.com>
2022-03-31 16:00:24 +0900

    🔫 File

---
+++ s3practice/src/main/java/com/example/s3practice/entity/File.java
@@ -0,0 +1,27 @@
+package com.example.s3practice.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+}
