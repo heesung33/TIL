4417a6a05513f046aef53765c526b35af7658eb9
heesung33 <chaeheesung33@gmail.com>
2022-06-16 22:49:56 +0900

    🔫 ToDoListRepository

--- todolist/src/main/java/com/example/todolist/domain/todolist/domain/repository/ToDoListRepository.java
+++ todolist/src/main/java/com/example/todolist/domain/todolist/domain/repository/ToDoListRepository.java
@@ -1,2 +1,8 @@
-package com.example.todolist.domain.todolist.domain.repository;public class ToDoListRepository {
+package com.example.todolist.domain.todolist.domain.repository;
+
+import com.example.todolist.domain.todolist.domain.ToDoList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ToDoListRepository extends JpaRepository<ToDoList, Long> {
 }
