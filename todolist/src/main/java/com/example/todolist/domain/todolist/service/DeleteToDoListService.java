03c404024449df91e7a90122a47dd08697e14e16
heesung33 <chaeheesung33@gmail.com>
2022-06-16 22:50:25 +0900

    DeleteToDoListService

--- todolist/src/main/java/com/example/todolist/domain/todolist/service/DeleteToDoListService.java
+++ todolist/src/main/java/com/example/todolist/domain/todolist/service/DeleteToDoListService.java
@@ -1,2 +1,18 @@
-package com.example.todolist.domain.todolist.service;public class DeleteToDoListService {
+package com.example.todolist.domain.todolist.service;
+
+import com.example.todolist.domain.todolist.domain.repository.ToDoListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteToDoListService {
+
+    private final ToDoListRepository toDoListRepository;
+
+    @Transactional
+    public void  execute(Long id) {
+        toDoListRepository.deleteById(id);
+    }
 }
