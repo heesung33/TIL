e2f98505852cad462e1da84a673a4b9f95227d0c
heesung33 <chaeheesung33@gmail.com>
2022-06-16 21:21:51 +0900

    🔫 PostRepository

--- crud/src/main/java/com/example/crud/entity/post/PostRepository.java
+++ crud/src/main/java/com/example/crud/entity/post/PostRepository.java
@@ -1,6 +1,5 @@
-package com.example.crud.entity.repository;
+package com.example.crud.entity.post;
 
-import com.example.crud.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
