0cbf870a9aa3110d38a12ade569ca3d25cadf08a
heesung33 <chaeheesung33@gmail.com>
2022-06-16 22:50:36 +0900

    GetToDoListService

--- todolist/src/main/java/com/example/todolist/domain/todolist/service/GetToDoListService.java
+++ todolist/src/main/java/com/example/todolist/domain/todolist/service/GetToDoListService.java
@@ -1,2 +1,24 @@
-package com.example.todolist.domain.todolist.service;public class GetToDoListService {
+package com.example.todolist.domain.todolist.service;
+
+import com.example.todolist.domain.todolist.domain.ToDoList;
+import com.example.todolist.domain.todolist.domain.repository.ToDoListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class GetToDoListService {
+
+    private final ToDoListRepository toDoListRepository;
+
+    @Transactional
+    public List<ToDoList> execute() {
+        return toDoListRepository.findAll();
+    }
+
+
 }
