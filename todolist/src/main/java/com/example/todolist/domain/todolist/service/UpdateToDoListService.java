88efec86072dd2cecec1702dff879e0e60b252ef
heesung33 <chaeheesung33@gmail.com>
2022-06-16 22:50:50 +0900

    🔫 UpdateToDoListService

--- todolist/src/main/java/com/example/todolist/domain/todolist/service/UpdateToDoListService.java
+++ todolist/src/main/java/com/example/todolist/domain/todolist/service/UpdateToDoListService.java
@@ -1,2 +1,22 @@
-package com.example.todolist.domain.todolist.service;public class UpdateToDoList {
+package com.example.todolist.domain.todolist.service;
+
+import com.example.todolist.domain.todolist.api.dto.request.ToDoRecord;
+import com.example.todolist.domain.todolist.domain.ToDoList;
+import com.example.todolist.domain.todolist.domain.repository.ToDoListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateToDoListService {
+
+    private final ToDoListRepository toDoListRepository;
+
+    @Transactional
+    public void execute(ToDoRecord record, Long id) {
+        ToDoList toDoList = toDoListRepository.findById(id).orElseThrow(RuntimeException::new);
+
+        toDoList.update(record.title(), record.content());
+    }
 }
