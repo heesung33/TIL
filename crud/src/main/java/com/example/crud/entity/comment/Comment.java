4c561cbfcc153dc6589a8b98abb9e594e082f2e2
heesung33 <chaeheesung33@gmail.com>
2022-06-20 14:59:07 +0900

    🔫 Comment

--- crud/src/main/java/com/example/crud/entity/comment/Comment.java
+++ crud/src/main/java/com/example/crud/entity/comment/Comment.java
@@ -1,32 +1,30 @@
 package com.example.crud.entity.comment;
 
+import com.example.crud.entity.BaseTimeEntity.BaseTimeEntity;
 import com.example.crud.entity.post.Post;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+
+
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Comment {
+public class Comment extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String userId;
+
     private String  comment;
 
-    @CreatedDate
-    private String createdDate;
-
-    @LastModifiedDate
-    private String modifiedDate;
-
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
     private Post post;
+
 }
