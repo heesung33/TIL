3ed1d7c51a4d5f54478bf8243277051b8879380e
heesung33 <chaeheesung33@gmail.com>
2022-06-20 15:13:56 +0900

    🔫 PostController

---
+++ crud/src/main/java/com/example/crud/api/PostController.java
@@ -0,0 +1,47 @@
+package com.example.crud.api;
+
+
+import com.example.crud.api.dto.request.PostRequest;
+import com.example.crud.api.dto.response.PostResponse;
+import com.example.crud.entity.post.Post;
+import com.example.crud.service.post.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostController {
+
+    private final PostCreateService postService;
+    private final PostDeleteService postDeleteService;
+    private final PostReadService postReadService;
+    private final PostGetService postGetService;
+    private final PostUpdateService postUpdateService;
+
+    @PostMapping("/post")
+    public void create(@RequestBody PostRequest postRequest) {
+      postService.execute(postRequest);
+    }
+
+    @GetMapping("/get")
+    public List<Post> read() {
+        return postReadService.execute();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) {
+        postDeleteService.execute(id);
+    }
+
+    @GetMapping("/{id}")
+    public PostResponse get(@PathVariable Long id) {
+        return postGetService.execute(id);
+    }
+
+    @PatchMapping("{id}")
+    public void update(@RequestBody PostRequest request, @PathVariable Long id) {
+        postUpdateService.execute(request, id);
+    }
+}
