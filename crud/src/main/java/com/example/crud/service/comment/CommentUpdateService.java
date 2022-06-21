3487f4387092614f739fda350e7cf007dfe2e7db
heesung33 <chaeheesung33@gmail.com>
2022-06-21 11:12:36 +0900

    🔫 CommentUpdateService 로직생성

--- crud/src/main/java/com/example/crud/service/comment/CommentUpdateService.java
+++ crud/src/main/java/com/example/crud/service/comment/CommentUpdateService.java
@@ -1,2 +1,26 @@
-package com.example.crud.service.comment;public class CommentUpdateService {
+package com.example.crud.service.comment;
+
+
+import com.example.crud.api.dto.request.CommentRequest;
+import com.example.crud.entity.comment.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentUpdateService {
+
+    private final CommentRepository commentRepository;
+
+
+    @Transactional
+    public void execute(CommentRequest commentRequest, long id) {
+        commentRepository.findById(id)
+                .map(newComment -> newComment.update(
+                        commentRequest.getComment(),
+                        commentRequest.getUserId()
+                ))
+                .orElseThrow(RuntimeException::new);
+    }
 }
