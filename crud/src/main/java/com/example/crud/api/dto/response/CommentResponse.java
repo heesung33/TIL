86128c6ec3636eff6c2f0d53f142d9e28cf27b75
heesung33 <chaeheesung33@gmail.com>
2022-06-21 15:36:29 +0900

    🔫 CommentResponse 수정

--- crud/src/main/java/com/example/crud/api/dto/response/CommentResponse.java
+++ crud/src/main/java/com/example/crud/api/dto/response/CommentResponse.java
@@ -3,10 +3,13 @@ package com.example.crud.api.dto.response;
 
 import com.example.crud.entity.comment.Comment;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class CommentResponse {
+
     private final Comment comment;
 }
