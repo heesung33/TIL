532f6caea3cbb0c4311a724d9178ff4c2e689ba0
heesung33 <chaeheesung33@gmail.com>
2022-06-21 15:35:57 +0900

    🔫 CommentController 수정

--- crud/src/main/java/com/example/crud/api/CommentController.java
+++ crud/src/main/java/com/example/crud/api/CommentController.java
@@ -2,11 +2,15 @@ package com.example.crud.api;
 
 import com.example.crud.api.dto.request.CommentRequest;
 import com.example.crud.api.dto.response.CommentResponse;
+import com.example.crud.entity.comment.Comment;
 import com.example.crud.service.comment.CommentDeleteService;
+import com.example.crud.service.comment.CommentGetService;
 import com.example.crud.service.comment.CommentUpdateService;
 import com.example.crud.service.comment.CommentWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +20,11 @@ public class CommentController {
     private final CommentWriteService commentWriteServiceService;
     private final CommentDeleteService commentDeleteService;
     private final CommentUpdateService commentUpdateService;
+    private final CommentGetService commentGetService;
 
     @PostMapping("/create")
-    public CommentResponse createComment(@RequestBody CommentRequest commentRequest) {
-        return commentWriteServiceService.execute(commentRequest);
+    public void createComment(@RequestBody CommentRequest commentRequest) {
+        commentWriteServiceService.execute(commentRequest);
     }
 
     @DeleteMapping("/delete/{id}")
@@ -31,5 +36,12 @@ public class CommentController {
     public void updateComment(@RequestBody CommentRequest commentRequest, @PathVariable long id) {
         commentUpdateService.execute(commentRequest, id);
     }
+
+
+    @GetMapping("/get")
+    public List<Comment> get(){
+        return commentGetService.execute();
+    }
+
 
 }
