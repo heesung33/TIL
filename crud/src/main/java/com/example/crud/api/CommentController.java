541218ce3c9fde58ad5cffebcd41570224c5783b
heesung33 <chaeheesung33@gmail.com>
2022-06-21 11:13:10 +0900

    🔫 CommentController

--- crud/src/main/java/com/example/crud/api/CommentController.java
+++ crud/src/main/java/com/example/crud/api/CommentController.java
@@ -3,6 +3,7 @@ package com.example.crud.api;
 import com.example.crud.api.dto.request.CommentRequest;
 import com.example.crud.api.dto.response.CommentResponse;
 import com.example.crud.service.comment.CommentDeleteService;
+import com.example.crud.service.comment.CommentUpdateService;
 import com.example.crud.service.comment.CommentWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ public class CommentController {
 
     private final CommentWriteService commentWriteServiceService;
     private final CommentDeleteService commentDeleteService;
+    private final CommentUpdateService commentUpdateService;
 
     @PostMapping("/create")
     public CommentResponse createComment(@RequestBody CommentRequest commentRequest) {
@@ -25,5 +27,9 @@ public class CommentController {
         commentDeleteService.execute(id);
     }
 
+    @PatchMapping("/update/{id}")
+    public void updateComment(@RequestBody CommentRequest commentRequest, @PathVariable long id) {
+        commentUpdateService.execute(commentRequest, id);
+    }
 
 }
