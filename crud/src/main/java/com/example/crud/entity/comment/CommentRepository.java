2ab05115dda34a3505600cb3a88e253b4130c924
heesung33 <chaeheesung33@gmail.com>
2022-06-21 15:37:39 +0900

    🔫 CommentRepository

--- crud/src/main/java/com/example/crud/entity/comment/CommentRepository.java
+++ crud/src/main/java/com/example/crud/entity/comment/CommentRepository.java
@@ -2,6 +2,8 @@ package com.example.crud.entity.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 }
