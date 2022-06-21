6a48624a40909f750fbb273b60cbde02b8db78f0
heesung33 <chaeheesung33@gmail.com>
2022-06-21 15:36:41 +0900

    🔫 CommentGetService 수정

--- crud/src/main/java/com/example/crud/service/comment/CommentGetService.java
+++ crud/src/main/java/com/example/crud/service/comment/CommentGetService.java
@@ -1,2 +1,30 @@
-package com.example.crud.service.comment;public class CommentGetService {
+
+package com.example.crud.service.comment;
+
+import com.example.crud.api.dto.response.CommentResponse;
+
+import com.example.crud.entity.comment.Comment;
+import com.example.crud.entity.comment.CommentRepository;
+
+import com.example.crud.entity.post.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class CommentGetService {
+
+    private final CommentRepository commentRepository;
+
+
+    @Transactional
+    public List<Comment> execute() {
+        return commentRepository.findAll();
+    }
+
 }
+
