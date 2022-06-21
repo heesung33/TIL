2b2a2f8cc838407671b5f66be53eb0e0beb5ddaf
heesung33 <chaeheesung33@gmail.com>
2022-06-21 15:37:23 +0900

    🔫 PostRepository

--- crud/src/main/java/com/example/crud/entity/post/PostRepository.java
+++ crud/src/main/java/com/example/crud/entity/post/PostRepository.java
@@ -2,8 +2,6 @@ package com.example.crud.entity.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Optional<Post> deleteByTitle(String title);
 }
