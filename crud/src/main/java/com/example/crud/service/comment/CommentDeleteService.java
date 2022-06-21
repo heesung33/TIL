8458f8f4667216eb3a93b04499ffa890b7200f87
heesung33 <chaeheesung33@gmail.com>
2022-06-21 10:57:51 +0900

    🔫 CommenteDeleteSerivce 수정

--- crud/src/main/java/com/example/crud/service/comment/CommentDeleteService.java
+++ crud/src/main/java/com/example/crud/service/comment/CommentDeleteService.java
@@ -1,2 +1,21 @@
-package com.example.crud.service.comment;public class CommentDeleteService {
+package com.example.crud.service.comment;
+
+import com.example.crud.entity.comment.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentDeleteService {
+
+    private final CommentRepository commentRepository;
+
+
+    @Transactional
+    public void execute(long id){
+        commentRepository.deleteById(id);
+    }
+
+
 }
