b69ab262baeb93d49933ee62b609f31b28f725d2
heesung33 <chaeheesung33@gmail.com>
2022-06-25 15:42:00 +0900

    🔫 ::  create

--- todolist/src/main/java/com/example/todolist/domain/comment/api/dto/request/CommentRecordRequset.java
+++ todolist/src/main/java/com/example/todolist/domain/comment/api/dto/request/CommentRecordRequset.java
@@ -3,7 +3,8 @@ package com.example.todolist.domain.comment.api.dto.request;
 
 import javax.validation.constraints.NotBlank;
 
-public record CommentRecord(
+
+public record CommentRecordRequset(
         @NotBlank String content
 ) {
 }
