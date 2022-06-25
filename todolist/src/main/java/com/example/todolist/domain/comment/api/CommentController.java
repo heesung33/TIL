4d0c84048ad97904531ab9261b93760d867e2d73
heesung33 <chaeheesung33@gmail.com>
2022-06-25 10:01:25 +0900

    🔫 :: 수정

--- todolist/src/main/java/com/example/todolist/domain/comment/api/CommentController.java
+++ todolist/src/main/java/com/example/todolist/domain/comment/api/CommentController.java
@@ -1,7 +1,8 @@
-package com.example.todolist.domain.todolist.api;
+package com.example.todolist.domain.comment.api;
 
-import com.example.todolist.domain.todolist.api.dto.request.CommentRecord;
-import com.example.todolist.domain.todolist.service.comment.CommentCreateService;
+import com.example.todolist.domain.comment.api.dto.request.CommentRecord;
+import com.example.todolist.domain.comment.service.CommentCreateService;
+import com.example.todolist.domain.comment.service.CommentUpdateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +12,18 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
 
     private final CommentCreateService commentCreateService;
+    private final CommentUpdateService commentUpdateService;
 
     @PostMapping("/create/{id}")
     public void create(@RequestBody CommentRecord commentRecord,
                        @PathVariable Long id) {
         commentCreateService.execute(commentRecord, id);
+    }
+
+    @PatchMapping("/update/{id}")
+    public void update(@RequestBody CommentRecord commentRecord,
+                       @PathVariable Long id) {
+        commentUpdateService.execute(commentRecord, id);
     }
 
 }
