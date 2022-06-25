6370f1fd7c86fa6a488aa78c3a6af747e7cc91ad
heesung33 <chaeheesung33@gmail.com>
2022-06-25 15:41:47 +0900

    🔫 ::  fix

--- todolist/src/main/java/com/example/todolist/domain/auth/domain/User.java
+++ todolist/src/main/java/com/example/todolist/domain/auth/domain/User.java
@@ -1,6 +1,8 @@
-package com.example.todolist.domain.auth.api.dto;
+package com.example.todolist.domain.auth.domain;
 
+import com.example.todolist.domain.auth.domain.types.Role;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,4 +27,14 @@ public class User {
     private String name;
 
     private Role role;
+
+    @Builder
+    public User(String email, String password, String name, Role role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+
+
 }
