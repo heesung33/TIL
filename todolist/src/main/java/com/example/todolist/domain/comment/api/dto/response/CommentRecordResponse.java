739e7fefa9974b76c9f8d36be4d5117795d9ac27
heesung33 <chaeheesung33@gmail.com>
2022-06-25 15:42:12 +0900

    🔫 :: fix

--- todolist/src/main/java/com/example/todolist/domain/comment/api/dto/response/CommentRecordResponse.java
+++ todolist/src/main/java/com/example/todolist/domain/comment/api/dto/response/CommentRecordResponse.java
@@ -2,14 +2,14 @@ package com.example.todolist.domain.comment.api.dto.response;
 
 
 import com.example.todolist.domain.todolist.domain.ToDoList;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 
-public record CommentRecord(
+
+public record CommentRecordResponse(
         @NotBlank ToDoList toDoList,
         @NotBlank String content
 ) {
