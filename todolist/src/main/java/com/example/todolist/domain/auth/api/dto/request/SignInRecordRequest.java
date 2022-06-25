dce840a08053e20d4abc0215fa73187be283340b
heesung33 <chaeheesung33@gmail.com>
2022-06-25 15:41:32 +0900

    🔫 :: fix

--- todolist/src/main/java/com/example/todolist/domain/auth/api/dto/request/SignInRecordRequest.java
+++ todolist/src/main/java/com/example/todolist/domain/auth/api/dto/request/SignInRecordRequest.java
@@ -2,7 +2,7 @@ package com.example.todolist.domain.auth.api.dto.request;
 
 import javax.validation.constraints.NotBlank;
 
-public record SignInRecord(
+public record SignInRecordRequest(
         @NotBlank String email,
         @NotBlank String password
 ) {
