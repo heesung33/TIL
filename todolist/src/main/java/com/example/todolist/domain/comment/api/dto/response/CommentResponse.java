f17d6edda9d19be9bc840a90a5f10feb8c1cde0e
heesung33 <chaeheesung33@gmail.com>
2022-06-25 14:37:37 +0900

    🔫 :: fix

--- todolist/src/main/java/com/example/todolist/domain/comment/api/dto/response/CommentResponse.java
+++ todolist/src/main/java/com/example/todolist/domain/comment/api/dto/response/CommentResponse.java
@@ -1,2 +1,15 @@
-package com.example.todolist.domain.comment.api.dto.response;public class CommentResponse {
+package com.example.todolist.domain.comment.api.dto.response;
+
+
+import com.example.todolist.domain.todolist.domain.ToDoList;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class CommentResponse {
+    private final ToDoList toDoList;
+    private final String content;
 }
