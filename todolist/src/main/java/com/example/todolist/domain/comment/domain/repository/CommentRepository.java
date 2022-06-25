ba72495634d6ba4cd6af392f422dfdc191cf6a02
heesung33 <chaeheesung33@gmail.com>
2022-06-25 14:37:26 +0900

    🔫 :: fix

--- todolist/src/main/java/com/example/todolist/domain/comment/domain/repository/CommentRepository.java
+++ todolist/src/main/java/com/example/todolist/domain/comment/domain/repository/CommentRepository.java
@@ -3,5 +3,11 @@ package com.example.todolist.domain.comment.domain.repository;
 import com.example.todolist.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByToDoListId(Long id);
+
 }
