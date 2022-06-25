1152a76d4b059fb3d16bfe26ee2a3426463a38e0
heesung33 <chaeheesung33@gmail.com>
2022-06-25 10:01:16 +0900

    🔫 :: 수정

--- crud/src/main/java/com/example/crud/service/comment/CommentGetService.java
+++ crud/src/main/java/com/example/crud/service/comment/CommentGetService.java
@@ -1,12 +1,11 @@
 
 package com.example.crud.service.comment;
 
-import com.example.crud.api.dto.response.CommentResponse;
 
 import com.example.crud.entity.comment.Comment;
 import com.example.crud.entity.comment.CommentRepository;
 
-import com.example.crud.entity.post.PostRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
