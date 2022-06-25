4d8fcf826d70af2a8b608f984e2d5e7738067fca
heesung33 <chaeheesung33@gmail.com>
2022-06-25 10:01:41 +0900

    🔫 :: 수정

--- todolist/src/main/java/com/example/todolist/domain/comment/service/CommentUpdateService.java
+++ todolist/src/main/java/com/example/todolist/domain/comment/service/CommentUpdateService.java
@@ -1,2 +1,23 @@
-package com.example.todolist.domain.comment.service;public class CommentUpdateService {
+package com.example.todolist.domain.comment.service;
+
+import com.example.todolist.domain.comment.api.dto.request.CommentRecord;
+import com.example.todolist.domain.comment.domain.Comment;
+import com.example.todolist.domain.comment.domain.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentUpdateService {
+
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public void execute(CommentRecord commentRecord, Long id) {
+        Comment comment = commentRepository.findById(id).orElseThrow(RuntimeException::new);
+
+        comment.update(commentRecord.content());
+    }
+
 }
