c3e779813e5663f0f02dcbf68e336a82a21bdfed
heesung33 <chaeheesung33@gmail.com>
2022-06-25 15:42:25 +0900

    🔫 :: fix

--- todolist/src/main/java/com/example/todolist/domain/comment/service/CommentGetService.java
+++ todolist/src/main/java/com/example/todolist/domain/comment/service/CommentGetService.java
@@ -1,7 +1,8 @@
 package com.example.todolist.domain.comment.service;
 
 
-import com.example.todolist.domain.comment.api.dto.response.CommentResponse;
+import com.example.todolist.domain.comment.api.dto.response.CommentRecordResponse;
+
 import com.example.todolist.domain.comment.domain.repository.CommentRepository;
 import com.example.todolist.domain.todolist.domain.ToDoList;
 import com.example.todolist.domain.todolist.domain.repository.ToDoListRepository;
@@ -21,17 +22,15 @@ public class CommentGetService {
     private final CommentRepository commentRepository;
     private final ToDoListRepository toDoListRepository;
 
+
     @Transactional
-    public List<CommentResponse> execute(Long id) {
+    public List<CommentRecordResponse> execute(Long id) {
         ToDoList toDoList = toDoListRepository.findById(id).orElseThrow(RuntimeException::new);
         return commentRepository.findAllByToDoListId(id)
                 .stream()
                 .map(comment -> {
-                    CommentResponse commentResponse = CommentResponse.builder()
-                            .toDoList(toDoList)
-                            .content(comment.getContent())
-                            .build();
-                    return commentResponse;
+                    CommentRecordResponse commentRecordResponse = new CommentRecordResponse(toDoList, comment.getContent());
+                    return commentRecordResponse;
                 })
                 .collect(Collectors.toList());
     }
