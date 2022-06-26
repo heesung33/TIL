3cc43239f3de21040317adb0f2d8b2150400f439
heesung33 <chaeheesung33@gmail.com>
2022-06-26 23:09:01 +0900

    🔫 :: create

---
+++ todolist/src/main/java/com/example/todolist/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.todolist.global.error;
+
+import com.example.todolist.global.error.exception.SnsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SnsException.class)
+    public ResponseEntity<ErrorResponse> handleException(SnsException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        return new ResponseEntity<>(new ErrorResponse(errorCode.getMessage()), HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(
+                new ErrorResponse(e.getBindingResult().getAllErrors().get(0).getDefaultMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
