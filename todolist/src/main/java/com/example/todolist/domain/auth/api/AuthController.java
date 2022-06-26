60a1ae75cd237e3db5dd8932975e004abf5de11b
heesung33 <chaeheesung33@gmail.com>
2022-06-26 23:10:12 +0900

    📝 :: revise

--- todolist/src/main/java/com/example/todolist/domain/auth/api/AuthController.java
+++ todolist/src/main/java/com/example/todolist/domain/auth/api/AuthController.java
@@ -1,6 +1,9 @@
 package com.example.todolist.domain.auth.api;
 
+import com.example.todolist.domain.auth.api.dto.request.SignInRecordRequest;
 import com.example.todolist.domain.auth.api.dto.request.SignUpRecordRequest;
+import com.example.todolist.domain.auth.api.dto.response.TokenRecordResponse;
+import com.example.todolist.domain.auth.service.AuthSignInService;
 import com.example.todolist.domain.auth.service.AuthSignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthSignUpService authSignUpService;
+    private final AuthSignInService authSignInService;
 
     @PostMapping("/signup")
     public void signup(@RequestBody SignUpRecordRequest signUpRecordRequest) {
         authSignUpService.execute(signUpRecordRequest);
+    }
+
+    @PostMapping("/signin")
+    public TokenRecordResponse signin(@RequestBody SignInRecordRequest signInRecordRequest){
+        return authSignInService.excute(signInRecordRequest);
     }
 }
