c5dac570ff698dcce7d348f55117754cfa2cf9f1
heesung33 <chaeheesung33@gmail.com>
2022-06-26 23:08:56 +0900

    🔫 :: create

---
+++ todolist/src/main/java/com/example/todolist/global/error/ExceptionHandlerFilter.java
@@ -0,0 +1,28 @@
+package com.example.todolist.global.error;
+
+import com.example.todolist.global.error.exception.SnsException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws ServletException, IOException {
+        try {
+            chain.doFilter(request, response);
+        } catch (SnsException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getMessage());
+
+            response.setStatus(e.getErrorCode().getStatus());
+            response.setContentType("application/json");
+            response.getWriter().write(errorResponse.convertToJson(errorResponse));
+        }
+    }
+}
