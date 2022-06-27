aee50c7273ea7785ae5069804c571f56230a9b32
heesung33 <chaeheesung33@gmail.com>
2022-06-27 14:58:11 +0900

    🔫 :: fix

--- todolist/src/main/java/com/example/todolist/domain/auth/api/AuthController.java
+++ todolist/src/main/java/com/example/todolist/domain/auth/api/AuthController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 @RestController
@@ -25,7 +27,7 @@ public class AuthController {
     }
 
     @PostMapping("/signin")
-    public TokenRecordResponse signin(@RequestBody SignInRecordRequest signInRecordRequest){
+    public TokenRecordResponse signin(@RequestBody @Valid SignInRecordRequest signInRecordRequest){
         return authSignInService.excute(signInRecordRequest);
     }
 }
