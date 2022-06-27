2e713fdb7373dfcc12ad5b3084cba343b2c66e6a
heesung33 <chaeheesung33@gmail.com>
2022-06-27 14:59:00 +0900

    🔫 :: fix

--- todolist/src/main/java/com/example/todolist/domain/comment/service/CommentCreateService.java
+++ todolist/src/main/java/com/example/todolist/domain/comment/service/CommentCreateService.java
@@ -1,11 +1,13 @@
 package com.example.todolist.domain.comment.service;
 
 
+import com.example.todolist.domain.auth.domain.User;
 import com.example.todolist.domain.comment.api.dto.request.CommentRecordRequset;
 import com.example.todolist.domain.comment.domain.Comment;
 import com.example.todolist.domain.comment.domain.repository.CommentRepository;
-import com.example.todolist.domain.todolist.domain.ToDoList;
-import com.example.todolist.domain.todolist.domain.repository.ToDoListRepository;
+import com.example.todolist.domain.post.domain.Post;
+import com.example.todolist.domain.post.domain.repository.PostRepository;
+import com.example.todolist.global.facade.CurrentFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,15 +18,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentCreateService {
 
     private final CommentRepository commentRepository;
-    private final ToDoListRepository toDoListRepository;
+    private final PostRepository toDoListRepository;
+    private final CurrentFacade currentFacade;
 
 
     @Transactional
     public void execute(CommentRecordRequset commentRecord, Long id) {
-        ToDoList toDoList = toDoListRepository.findById(id).orElseThrow(RuntimeException::new);
+        Post toDoList = toDoListRepository.findById(id).orElseThrow(RuntimeException::new);
+
+        User user = currentFacade.getCurrentUser();
 
         commentRepository.save(
                 Comment.builder()
+                        .user(user)
                         .toDoList(toDoList)
                         .content(commentRecord.content())
                         .build()
