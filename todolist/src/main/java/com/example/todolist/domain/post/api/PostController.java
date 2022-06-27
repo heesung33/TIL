7117ddbe78f470321a37511de04ed244be2e0217
heesung33 <chaeheesung33@gmail.com>
2022-06-27 14:59:03 +0900

    🔫 :: fix

--- todolist/src/main/java/com/example/todolist/domain/post/api/PostController.java
+++ todolist/src/main/java/com/example/todolist/domain/post/api/PostController.java
@@ -1,8 +1,8 @@
 package com.example.todolist.domain.post.api;
 
-import com.example.todolist.domain.post.api.dto.request.ToDoRecord;
-import com.example.todolist.domain.post.api.dto.response.ToDoResponse;
-import com.example.todolist.domain.post.domain.ToDoList;
+import com.example.todolist.domain.post.api.dto.request.PostRecord;
+import com.example.todolist.domain.post.api.dto.response.PostResponse;
+import com.example.todolist.domain.post.domain.Post;
 import com.example.todolist.domain.post.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/post")
-public class ToDoListController {
+public class PostController {
 
     private final CreateToDoListService createToDoListService;
     private final GetToDoListService getToDoListService;
@@ -22,12 +22,12 @@ public class ToDoListController {
     private final GetToDoService getToDoService;
 
     @PostMapping("/create")
-    public void createToDoList(@RequestBody @Valid ToDoRecord record) {
+    public void createToDoList(@RequestBody @Valid PostRecord record) {
             createToDoListService.execute(record);
     }
 
     @GetMapping("/list")
-    public List<ToDoList> getToDoList(){
+    public List<Post> getToDoList(){
         return getToDoListService.execute();
     }
 
@@ -37,12 +37,12 @@ public class ToDoListController {
     }
 
     @PatchMapping("/update/{id}")
-    public void patchToDoList(@RequestBody ToDoRecord record, @PathVariable Long id) {
+    public void patchToDoList(@RequestBody PostRecord record, @PathVariable Long id) {
         updateToDoListService.execute(record, id);
     }
 
     @GetMapping("/getall/{id}")
-    public ToDoResponse getAllToDo(@PathVariable Long id) {
+    public PostResponse getAllToDo(@PathVariable Long id) {
         return getToDoService.execute(id);
     }
 }
