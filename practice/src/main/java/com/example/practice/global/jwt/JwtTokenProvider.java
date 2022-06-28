d201feee32c560d8dde5e5e1f0ad45dec58b2235
heesung33 <chaeheesung33@gmail.com>
2022-06-28 23:58:38 +0900

    🔫 :: create

---
+++ practice/src/main/java/com/example/practice/global/jwt/JwtTokenProvider.java
@@ -0,0 +1,19 @@
+package com.example.practice.global.jwt;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    private static final String ACCESS_KEY = "access";
+    private static final String REFRESH_KEY = "refresh";
+    private static final String HEADER = "Authorization";
+    private static final String PREFIX = "Bearer ";
+
+
+
+
+}
