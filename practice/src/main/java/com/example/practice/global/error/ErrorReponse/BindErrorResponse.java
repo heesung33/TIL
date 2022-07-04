9f977b87838a3948b035ec1d9a5cf66c388d51d7
heesung33 <chaeheesung33@gmail.com>
2022-07-04 21:41:44 +0900

    🔫 :: create

---
+++ practice/src/main/java/com/example/practice/global/error/ErrorReponse/BindErrorResponse.java
@@ -0,0 +1,17 @@
+package com.example.practice.global.error.ErrorReponse;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class BindErrorResponse {
+
+    private final int status;
+    private final List<String> messages;
+}
