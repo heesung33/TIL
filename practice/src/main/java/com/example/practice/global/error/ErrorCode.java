e4a04564d318bcb14d6bbb94b611a678d819a10e
heesung33 <chaeheesung33@gmail.com>
2022-07-05 23:25:58 +0900

    🔫 :: fix

--- practice/src/main/java/com/example/practice/global/error/ErrorCode.java
+++ practice/src/main/java/com/example/practice/global/error/ErrorCode.java
@@ -8,7 +8,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode{
-    AUTH_NOT_FOUND(404, "Auth Not Found");
+    PASSWORD_MISMATCH(401, "Password MisMatch"),
+    EXPIRED_JWT(401, "Expired Jwt"),
+    AUTH_NOT_FOUND(404, "Auth Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    SIGNATURE_JWT(401, "Signature Jwt"),
+    INVALID_JWT(401, "Invalid Jwt");
 
     private final int status;
     private final String message;
