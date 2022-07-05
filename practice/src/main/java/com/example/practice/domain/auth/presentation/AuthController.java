9bdc0cbc8020d913279ef69bb752d65639f0ece8
heesung33 <chaeheesung33@gmail.com>
2022-07-05 23:25:43 +0900

    🔫 :: fix

--- practice/src/main/java/com/example/practice/domain/auth/presentation/AuthController.java
+++ practice/src/main/java/com/example/practice/domain/auth/presentation/AuthController.java
@@ -1,6 +1,9 @@
 package com.example.practice.domain.auth.presentation;
 
 
+import com.example.practice.domain.auth.domain.Role;
+import com.example.practice.domain.auth.presentation.dto.request.SignInRequest;
+import com.example.practice.domain.auth.service.SignInService;
 import com.example.practice.domain.auth.service.SignUpService;
 import com.example.practice.domain.auth.presentation.dto.request.SignUpRequest;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +12,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/auth")
 @RestController
 public class AuthController {
 
     private final SignUpService signUpService;
+    private final SignInService signInService;
 
     @PostMapping("/signup")
     public void signup(@RequestBody SignUpRequest signUpRequest) {
         signUpService.execute(signUpRequest);
+    }
+
+    @PostMapping("/signin")
+    public void signin(@RequestBody @Valid SignInRequest signInRequest) {
+        signInService.execute(signInRequest);
     }
 }
