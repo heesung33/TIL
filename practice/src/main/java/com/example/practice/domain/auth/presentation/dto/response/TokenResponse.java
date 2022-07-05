2ebbe766288c0fb94612848660a561385d3a8255
heesung33 <chaeheesung33@gmail.com>
2022-07-05 23:25:50 +0900

    🔫 :: fix

--- practice/src/main/java/com/example/practice/domain/auth/presentation/dto/response/TokenResponse.java
+++ practice/src/main/java/com/example/practice/domain/auth/presentation/dto/response/TokenResponse.java
@@ -1,10 +1,12 @@
 package com.example.practice.domain.auth.presentation.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class TokenResponse {
-    private String accessToken;
-    private String refreshToken;
+    private final String accessToken;
+    private final String refreshToken;
 
 }
