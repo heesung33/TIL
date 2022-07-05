fd947896cdaef1d54ea0b7acb225fbffa74927d1
heesung33 <chaeheesung33@gmail.com>
2022-07-05 23:26:08 +0900

    🔫 :: fix

--- practice/src/main/java/com/example/practice/domain/auth/service/SignInService.java
+++ practice/src/main/java/com/example/practice/domain/auth/service/SignInService.java
@@ -1,9 +1,13 @@
 package com.example.practice.domain.auth.service;
 
 import com.example.practice.domain.auth.domain.Auth;
+import com.example.practice.domain.auth.domain.Role;
 import com.example.practice.domain.auth.domain.repository.AuthRepository;
+import com.example.practice.domain.auth.exception.PasswordMisMatchException;
 import com.example.practice.domain.auth.presentation.dto.request.SignInRequest;
 import com.example.practice.domain.auth.presentation.dto.response.TokenResponse;
+import com.example.practice.global.exception.AuthNotFoundException;
+import com.example.practice.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,18 +18,30 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class SignInService {
 
-    private final AuthRepository userRepository;
+    private final AuthRepository authRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @Transactional
-    public TokenResponse excute(SignInRequest signInRequest) {
-        Auth user = userRepository.findByAccountId(signInRequest.getAccountId())
-                .orElseThrow(RuntimeException::new);
 
-        if(!passwordEncoder.matches(signInRequest.getPassword(), signInRequest.getPassword()))
-            throw new RuntimeException();
+    public TokenResponse execute(SignInRequest request) {
+        return signIn(request, Role.USER);
+    }
 
-        TokenResponse tokenResponse =
+
+    private TokenResponse signIn(SignInRequest request, Role role) {
+        Auth auth = authRepository.findByAccountId(request.getAccountId())
+                .orElseThrow(() -> AuthNotFoundException.EXCEPTION);
+
+        if (!passwordEncoder.matches(request.getPassword(), auth.getPassword())) {
+            throw PasswordMisMatchException.EXCEPTION;
+        }
+
+        TokenResponse tokenResponse = jwtTokenProvider.generateTokens(request.getAccountId(), role);
+
+        return TokenResponse.builder()
+                .accessToken(tokenResponse.getAccessToken())
+                .refreshToken(tokenResponse.getRefreshToken())
+                .build();
     }
 
 
